f89dc97593b84dab132e5c2e102fccc58ffe1c4c
Martí Arnau <marnauortega@gmail.com>
2023-07-30 19:05:41 +0200

    fix(middleware): netlify middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { locales as localesArray } from "./lib/locales";
+import { MiddlewareRequest } from "@netlify/next";
 
 let locales = localesArray.map((language) => language.lang);
 const defaultLocale = locales[0];
@@ -9,11 +10,10 @@ export async function middleware(req) {
   const searchParams = req.nextUrl.search;
   const locale = pathname.split("/")[1];
 
-  const alreadyRewritten = req.headers.get("x-middleware-rewrite");
-  console.log(req.headers);
+  const middlewareRequest = new MiddlewareRequest(req);
 
   // Check if the default locale is in the pathname
-  if (defaultLocale === locale && !alreadyRewritten) {
+  if (defaultLocale === locale) {
     console.log("remove default locale");
     // we remove locale if pathname contains default locale, by redirecting
     if (pathname.startsWith(`/${locale}/`)) {
@@ -30,13 +30,13 @@ export async function middleware(req) {
   });
 
   if (pathnameIsMissingValidLocale) {
-    console.log("hide default locale, but add it");
+    console.log("hide default locale, but add it", new URL("/ca", req.url));
     // we rewrite pathnames without valid locale: visit `/ca`, but browser url shows `/`
     // Incoming request: "/hello"
     // Rewritten request: page shown "/en/hello", browser url "/hello"
-    return NextResponse.rewrite(
+    return middlewareRequest.rewrite(
       // Pathname already includes "/"
-      new URL(`/${defaultLocale}${pathname}${searchParams}`, req.url)
+      new URL("/ca", req.url)
     );
   }
 }

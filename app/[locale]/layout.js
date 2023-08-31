8aa72930c62fd6e1b7a1621fb5d627eb245c943c
Martí Arnau <marnauortega@gmail.com>
2023-08-31 11:05:00 +0200

    fix(link): link doesn't trigger middleware

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
+export const dynamic = "force-static";
+export const revalidate = 60;
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/ca">Català</Link>
+            </li>
+            <li>
+              <Link href="/es">Español</Link>
+            </li>
+            <li>
+              <Link href="/en">English</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ let locales = localesArray.map((language) => language.lang);
 const defaultLocale = locales[0];
 
 export async function middleware(req) {
-  const pathname = req.nextUrl.pathname;
+  let pathname = req.nextUrl.pathname;
   const searchParams = req.nextUrl.search;
   const locale = pathname.split("/")[1];
 
@@ -14,30 +14,37 @@ export async function middleware(req) {
 
   // Check if the default locale is in the pathname
   if (defaultLocale === locale) {
-    console.log("remove default locale");
+    console.log("default locale: ", defaultLocale, "locale: ", locale, "they are equal");
     // we remove locale if pathname contains default locale, by redirecting
     if (pathname.startsWith(`/${locale}/`)) {
+      console.log("pathname starts with /locale/ (because it has another param), so '/locale' will be replaced");
       // if pathname has subpath, remove "/locale", eg. from "/ca/something" to "/something"
       return NextResponse.redirect(new URL(`${pathname.replace(`/${locale}`, "")}${searchParams}`, req.url));
     } else {
+      console.log(
+        "pathname doesn't start with /locale/ (because it only contains the locale param), so 'locale' will be replaced"
+      );
       // if it doesn't, remove "locale", eg. from "/ca to "/"
       return NextResponse.redirect(new URL(`${pathname.replace(`${locale}`, "")}${searchParams}`, req.url));
     }
   }
 
   const pathnameIsMissingValidLocale = locales.every((locale) => {
-    return !pathname.startsWith(`/${locale}`);
+    // If pathname isn't "/ca" and doesn't contain "/ca/", then it isn't valid
+    return pathname !== `/${locale}` && !pathname.startsWith(`/${locale}/`);
   });
 
   if (pathnameIsMissingValidLocale) {
-    console.log("hide default locale, but add it", new URL("/ca", req.url));
+    console.log(
+      `pathname has no valid locale, thus it will be rewriten. page shown '/${defaultLocale}${pathname}', browser url '${pathname}'`
+    );
+    // Remove trailing slash
+    if (pathname === "/") pathname = "";
     // we rewrite pathnames without valid locale: visit `/ca`, but browser url shows `/`
     // Incoming request: "/hello"
     // Rewritten request: page shown "/en/hello", browser url "/hello"
-    // And remove trailing slash
-    if (pathname === "/") pathname = "";
     return middlewareRequest.rewrite(
-      // Pathname already includes "/"
+      // Note: Pathname already includes "/"
       new URL(`/${defaultLocale}${pathname}${searchParams}`, req.url)
     );
   }
